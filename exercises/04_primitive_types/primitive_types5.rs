a8dc34d101bebbe54e849a1e9e5447b808f00251
KevinM <kevinmorrissey2010@yahoo.com>
2024-09-09 13:13:02 -0400

    completed rustlings primitive types exercises

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -1,6 +1,9 @@
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-
+    let (name, age) = cat;
+    //further look at tupling shows I don't have to structure it by declaration and then assigning, can just be done in the tuple destructure.
+    //let name = x;
+   // let age = y;
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
 
