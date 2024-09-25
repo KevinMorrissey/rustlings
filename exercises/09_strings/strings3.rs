d05cb1f5be51f5948a97f3463abf5f938309594d
KevinM <kevinmorrissey2010@yahoo.com>
2024-09-25 10:13:37 -0400

    strings completed

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,12 @@
-fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+fn trim_me(input: &str) -> String{
+    input.trim().to_string()
 }
-
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
